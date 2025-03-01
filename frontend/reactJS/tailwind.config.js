8f69a9f7d9b88bf7e4180a302e453a13690fd03a
Samir34elk <122782097+Samir34elk@users.noreply.github.com>
2025-03-01 07:55:32 +0100

    Create tailwind.config.js

---
+++ frontend/reactJS/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Assurez-vous que tous les fichiers où Tailwind sera utilisé sont inclus
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
