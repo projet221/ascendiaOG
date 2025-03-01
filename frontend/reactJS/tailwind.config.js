afd25227b566196229953cae39b66893efea3719
Samir34elk <122782097+Samir34elk@users.noreply.github.com>
2025-03-01 21:13:43 +0100

    Update tailwind.config.js

--- frontend/reactJS/tailwind.config.js
+++ frontend/reactJS/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}", // Assurez-vous que tous les fichiers où Tailwind sera utilisé sont inclus
+    "./src/**/*.{html,js,jsx,css}", // Assurez-vous que tous les fichiers où Tailwind sera utilisé sont inclus
   ],
   theme: {
     extend: {},
