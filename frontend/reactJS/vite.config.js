3e0f1fde61d53df73565c536187add66e4bbae6a
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-03-03 17:33:34 +0100

    recuperation des modifs

--- frontend/reactJS/vite.config.js
+++ frontend/reactJS/vite.config.js
@@ -1,19 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    Pages({ dirs: 'src/pages' }),
-  ],
-  build: {
-    target: 'esnext',
-  },
-  server: {
-    proxy: {
-      '/api': 'https://gateway-production-207f.up.railway.app',
+    plugins: [
+        react(),
+        Pages({dirs: 'src/pages'}),
+        tailwindcss(),
+    ],
+    build: {
+        target: 'esnext',
     },
-  },
+    server: {
+        proxy: {
+            '/api': "http://localhost:3000",
+        },
+        https: {
+            key: fs.readFileSync("localhost-key.pem"),
+            cert: fs.readFileSync("localhost.pem"),
+        },
+        host: "localhost",
+        // Change si besoin
+
+    },
 })
 
