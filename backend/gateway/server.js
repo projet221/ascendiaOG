b0c175e0ef9106ad79d18c327310b7edc70d5509
projet221 <projetl3ter@gmail.com>
2025-03-18 20:42:59 +0100

    Update server.js

--- backend/gateway/server.js
+++ backend/gateway/server.js
@@ -13,6 +13,16 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log('Requête reçue :', {
+        method: req.method, // Méthode HTTP (GET, POST, etc.)
+        url: req.originalUrl, // URL complète de la requête
+        headers: req.headers, // En-têtes de la requête
+        body: req.body, // Corps de la requête (pour les méthodes POST, PUT, etc.)
+    });
+    next(); // Passer à la suite
+});
+
 // Proxy configuration
 app.use('/api/users', proxy(process.env.PROXY_USERS, {timeout: 10000}));
 app.use('/api/socialauth', proxy(process.env.PROXY_SOCIALAUTH, {timeout: 10000}));
