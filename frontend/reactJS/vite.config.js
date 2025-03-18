da5bfd43cf01a29407ca063403976914bc1b502d
projet221 <projetl3ter@gmail.com>
2025-03-18 20:20:54 +0100

    Update vite.config.js

--- frontend/reactJS/vite.config.js
+++ frontend/reactJS/vite.config.js
@@ -1,35 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import Pages from 'vite-plugin-pages';
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
 import * as fs from "node:fs";
 
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log("PROXY_GATEWAY:", process.env.VITE_PROXY_GATEWAY);
 
 export default defineConfig({
-  plugins: [
-    react(),
-    Pages({ dirs: 'src/pages' }),
-  ],
-  build: {
-    target: 'esnext',
-  },
-  server: {
-    proxy: {
-      // Proxy toutes les requêtes /api vers l'URL définie dans la variable d'environnement PROXY_GATEWAY
-      '/api': {
-        target: process.env.PROXY_GATEWAY,
-        changeOrigin: true, // Si tu as besoin de changer l'origine de la requête (utile pour les APIs avec CORS)
-        rewrite: (path) => path.replace(/^\/api/, ''), // Supprime le préfixe /api pour l'API
-      },
-      // Redirection spécifique vers Facebook OAuth
-      '/api/socialauth/connect/facebook': {
-        target: 'https://www.facebook.com/v12.0/dialog/oauth',
-        changeOrigin: true,
-        rewrite: (path) => path, // Pas besoin de modifier ici, la redirection se fait telle quelle
-        ws: false,  // Pas de WebSocket nécessaire pour cette redirection
-      },
-    },
-  },
-});
+        plugins: [
+            react(),
+            Pages({dirs: 'src/pages'}),
+        ],
+        build: {
+            target: 'esnext',
+        },
+        server: {
+            proxy: {
+                '/api': `${process.env.VITE_PROXY_GATEWAY}`,
+            },
+        },
+    }
+)
+
