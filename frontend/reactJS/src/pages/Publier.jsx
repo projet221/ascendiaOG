0818bf36c49c15f3c41ee1c5075341b9197f9916
Hachem <aly.hachem-reda@etu.umontpellier.fr>
2025-03-20 16:10:07 +0100

    ajout de page publier

---
+++ frontend/reactJS/src/pages/Publier.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import SelectCompte from "../components/SelectCompte.jsx";
+import AjoutFichierBouton from "../components/AjoutFichierBouton.jsx";
+
+function Publier(){
+
+
+    function publier(){
+
+    }
+    return (
+        <div className="flex items-center justify-center">
+            <div className="grid item-center bg-white ">
+                <SelectCompte/>
+            </div>
+                <AjoutFichierBouton/>
+            <div>
+                <textarea name="post" id="messagePost">Ecrivez message</textarea>
+            </div>
+            <div>
+                
+            </div>
+            <label htmlFor="publish-select">Choisissez une action :</label>
+            <select id="publish-select"  onChange={publier()}>
+                <option value="" disabled>-- Sélectionnez une option --</option>
+                <option value="maintenant">Publier Maintenant</option>
+                <option value="planifier">Planifier</option>
+                <option value="brouillon">Enregistrer en brouillon</option>
+            </select>
+        </div>
+    );
+}
+export default Publier;
