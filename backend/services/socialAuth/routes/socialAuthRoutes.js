4c8e53c3bdcb0408cc2477f34bc0e49730888549
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-03-21 02:14:10 +0100

    maj récup username pour dashboard

--- backend/services/socialAuth/routes/socialAuthRoutes.js
+++ backend/services/socialAuth/routes/socialAuthRoutes.js
@@ -5,17 +5,7 @@ const router = express.Router();
 const axios = require("axios");
 // Facebook OAuth
 
-router.get("/connect/facebook", (req, res, next) => {
-    const user_id = req.query.user_id; // Récupération du user_id depuis l'URL
-
-    if (!user_id) {
-        return res.status(400).json({ error: "user_id est requis" });
-    }
-
-    // Stocker user_id dans la session
-    req.session.user_id = user_id;
-
-    // Rediriger vers l'authentification Facebook
+router.get("/connect/facebook",
     passport.authenticate("facebook", {
         scope: [
             "email", "public_profile", "pages_read_user_content", "read_insights", "pages_show_list",
@@ -23,57 +13,20 @@ router.get("/connect/facebook", (req, res, next) => {
             "instagram_basic", "instagram_content_publish", "ads_management", "instagram_manage_insights",
             "ads_read"
         ]
-    })(req, res, next); // Exécuter Passport immédiatement
-});
-router.get("/connect/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/" }),
-    async (req, res) => {
-        /*
-        const { code } = req.query;
-       // const userId = req.cookies.user_id; // Récupérer le user_id depuis le cookie
+    })
+);
 
-                if (!userId) {
-                    return res.status(400).json({ error: "user_id manquant !" });
-                }
-
-                if (!code) {
-                    return res.status(400).json({ error: "Code d'autorisation manquant !" });
-                }
-                console.log(code);
-
-                try {
-                    const response = await axios.get(`https://graph.facebook.com/v12.0/oauth/access_token`, {
-                        params: {
-                            client_id: process.env.FACEBOOK_CLIENT_ID,
-                            client_secret: process.env.FACEBOOK_CLIENT_SECRET,
-                            redirect_uri: process.env.PROXY_GATEWAY + "/api/socialauth/connect/facebook/callback",
-                            code: code,
-                        }
-                    });
-
-                   const { access_token } = response.data;
-                    console.log(access_token);
-
-                    const userInfoResponse = await axios.get(`https://graph.facebook.com/me`, {
-                        params: {
-                            access_token: access_token,
-                            fields: 'id,name,email',
-                        }
-                    });
-
-
-            await SocialAuth.findOneAndUpdate(
-                { provider: "facebook" },
-                { accessToken: access_token, refreshToken: userInfoResponse.toString()  },
-                { upsert: true, new: true }
-            );
-
-            res.send("<script>window.close();</script>");
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: "Erreur lors de l'échange du code ou de l'enregistrement du token :" });
-        }*/
-        res.send("<script>window.close();</script>");
+router.get("/connect/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }),
+    (req, res) => {
+    if (!req.user || !req.user.accessToken) {
+        console.log("erreur lors de la recuperation de l'acces token")
     }
+
+    const facebookAccessToken = req.user.accessToken; // Le token récupéré depuis Facebook
+
+    // Rediriger vers le frontend avec le token dans l'URL
+    res.redirect(process.env.FRONTEND_URL + `/socialauth/callback?facebook_token=${facebookAccessToken}`);
+}
 );
 
 router.get('/connect/twitter', (req, res) => {
