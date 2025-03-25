d9e14141c64fe17a3b7b804c91d6d554ae5253ca
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-03-25 06:23:52 +0100

    update model SocialAuth.js

--- backend/services/socialAuth/models/SocialAuth.js
+++ backend/services/socialAuth/models/SocialAuth.js
@@ -6,17 +6,9 @@ const SocialAuth = new mongoose.Schema({
     accessToken: {type: String, required: true}, // Token d'accès pour API externe
     refreshToken: {type: String}, // Optionnel, utile pour rafraîchir les tokens
     secretToken: {type: String}, // Optionnel, utile pour twitter
-    pages: [{
-        name: {type: String, required: true},  // Nom de la page
-        id: {type: String, required: true}     // ID de la page Facebook
-    }],
+    pages: { type: [{ name: String, id: String }] },
     createdAt: {type: Date, default: Date.now}
 });
-
-// Ajout de la validation conditionnelle pour 'pages'
-SocialAuth.path('pages').validate(function(value) {
-    return value === undefined || value.length > 0;
-}, 'Pages array cannot be empty.');
 
 
 module.exports = mongoose.model("SocialAuth", SocialAuth);
