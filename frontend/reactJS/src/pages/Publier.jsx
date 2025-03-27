56ff55a2f7e50afb7490358aad3388acd1080501
sami <sami.chbicheb@etu.umontpellier.fr>
2025-03-27 02:57:40 +0100

    modif rajout middleware upload

--- frontend/reactJS/src/pages/Publier.jsx
+++ frontend/reactJS/src/pages/Publier.jsx
@@ -35,7 +35,7 @@ function Publier() {
             if(fichier){
                 formData.append("file", fichier);
             }
-            console.log("le formdata file",formData.file);
+            console.log("le formdata file",formData );
             const response = await axiosInstance.post(
                 "/api/posts",
                 formData,
