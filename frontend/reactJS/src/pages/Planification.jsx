36f5b784afca9bdebe921cdcc4072b805ec42364
so5o <sofiane.hammar@etu.umontpellier.fr>
2025-03-28 18:18:53 +0100

    jj

---
+++ frontend/reactJS/src/pages/Planification.jsx
@@ -0,0 +1,11 @@
+const Planification = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold text-center mb-4" style={{ color: '#FF0035' }}>
+        Page Planification
+      </h1>
+    </div>
+  );
+};
+
+export default Planification;
