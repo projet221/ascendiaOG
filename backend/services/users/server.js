27e352de1ec52a5f2fd99aa4c9a24d3ee91218d6
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-03-29 22:45:20 +0100

    debug server.js de tout les services

--- backend/services/users/server.js
+++ backend/services/users/server.js
@@ -21,7 +21,7 @@ connectDB();
 app.use(userRoutes);
 
 // Error handling middleware
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({error: 'Something went wrong!'});
 });
