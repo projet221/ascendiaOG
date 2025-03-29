907f488a524fed97a9a195d4b952a3c8f4c5283a
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-03-29 21:25:52 +0100

    redirection dashboard si déja connecté

--- frontend/reactJS/src/pages/Index.jsx
+++ frontend/reactJS/src/pages/Index.jsx
@@ -1,30 +1,19 @@
-import {useState} from "react";
-import BarreHaut from "../components/BarreHaut.jsx";
-import ConfigSocialMedia from "../components/ConfigSocialMedia.jsx";
-import Publier from "./Publier.jsx" ;
+import { useNavigate } from "react-router-dom"; // Ajout de l'import
 import HomePage from "./HomePage.jsx";
 
 const Index = () => {
-    // État local pour gérer la connexion
-    const [connexion, setConnexion] = useState(true);
 
-    // Fonction pour inverser l'état de connexion
-    const toggleConnexion = () => {
-        setConnexion((prevConnexion) => !prevConnexion);
-    };
     const token = localStorage.getItem("token");
+    const navigate = useNavigate(); // Correction ici
+
+    if (token) {
+        navigate("/dashboard");
+    }
 
     return (
         <div>
-            <head><link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-/>
-</head>
-            <HomePage/>
-        
-        
-            </div>
+            <HomePage />
+        </div>
     );
 };
 
