97e9dcf42da97d449d750d525d889243d5764b62
so5o <sofiane.hammar@etu.umontpellier.fr>
2025-04-05 02:56:54 +0200

    all

--- backend/services/posts/routes/postRoutes.js
+++ backend/services/posts/routes/postRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const upload = require('../middlewares/upload'); 
 const router = express.Router();
 const postController = require('../controllers/postController');
-const { getInstagramPostsByUser } = require("../controllers/instagram/functions");
+
 
 router.get('/:networks/:id', postController.getAllPosts);
 router.post('/',upload.single('file'), postController.createPost);
@@ -13,8 +13,7 @@ router.delete('/:id', postController.deletePost);
 router.get('/api/users/:userId/posts', postController.getUserPosts);
 //router.put('/:id/schedule', postController.schedulePost);
 router.put('/:id/analytics', postController.updateAnalytics);
-router.get("/users/:userId/scheduled", postController.getScheduledPostsByUser);
-router.get("/instagram/:userId", getInstagramPostsByUser);
+
 
 
 module.exports = router; //c est har tt le monde a ce probleme mais personne sait comment résoudre
