3e0664058dc5f4c481bd9a5b577ccbd2c4b8bd4b
so5o <sofiane.hammar@etu.umontpellier.fr>
2025-04-05 03:10:51 +0200

    stats

--- frontend/reactJS/src/pages/Statistiques.jsx
+++ frontend/reactJS/src/pages/Statistiques.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import BarreHaut from "../components/BarreHaut";
+import SidebarPublication from "../components/SideBarPublication";
 import StatistiquesContent from "../components/StatistiquesContent";
 
 const Statistiques = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold text-center mb-4" style={{ color: '#FF0035' }}>
-        Page Statistiques
-      </h1>
-      <StatistiquesContent />
+    <div>
+      <BarreHaut />
+      <div className="flex">
+        <SidebarPublication />
+        <main className="flex-1 ml-64 mt-16 p-6 bg-gray-50 min-h-screen">
+          <h1 className="text-3xl font-bold text-[#FF0035] mb-6 text-center">
+            Statistiques
+          </h1>
+          <StatistiquesContent />
+        </main>
+      </div>
     </div>
   );
 };
