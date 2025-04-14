4bb8c17235dd110e9f1067ff895395b7be503a12
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-04-14 22:26:56 +0200

    debug Dashboard.jsx

--- frontend/reactJS/src/pages/Dashboard.jsx
+++ frontend/reactJS/src/pages/Dashboard.jsx
@@ -82,7 +82,7 @@ export default function Dashboard() {
     function formatDate(dateString) {
         const date = new Date(dateString);
         return `${date.getDate().toString().padStart(2, '0')}/${
-            (date.getMonth() + 1).toString().padStart(2, '0')}/${
+            (date.getMonth()).toString().padStart(2, '0')}/${
             date.getFullYear()} ${
             date.getHours().toString().padStart(2, '0')}:${
             date.getMinutes().toString().padStart(2, '0')}`;
