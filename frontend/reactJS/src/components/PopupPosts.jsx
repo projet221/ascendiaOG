c272b41a47f46e1909c8d99d6036c36a953012d7
sami <sami.chbicheb@etu.umontpellier.fr>
2025-04-16 23:31:26 +0200

    calendrier avec posts a venir

--- frontend/reactJS/src/components/PopupPosts.jsx
+++ frontend/reactJS/src/components/PopupPosts.jsx
@@ -2,17 +2,14 @@
 import React, { useState,useEffect } from "react";
 
 const PopupPosts = ({ isOpen, onClose, posts, date }) => {
+    if (!isOpen) return null;
     const [datePosts,setDatePosts] = useState(
         posts.filter((element)=>{
         element.scheduledFor.split("T")[0] == date; //on ne garde que les posts dont la date de publication programmé est celle de la cellule
     }));
     console.log(datePosts);
-  if (!isOpen) return null;
-   useEffect(()=>{
-    const res = posts.filter((element)=>{
-        element.scheduledFor.split("T")[0] == date;
-    });
-   },[]); 
+
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg relative">
