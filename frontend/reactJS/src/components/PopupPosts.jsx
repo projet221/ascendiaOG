05c2e35fa3f6e2812e1674c16dde6fc642be20dc
sami <sami.chbicheb@etu.umontpellier.fr>
2025-04-17 00:38:18 +0200

    calendrier avec posts a venir

--- frontend/reactJS/src/components/PopupPosts.jsx
+++ frontend/reactJS/src/components/PopupPosts.jsx
@@ -8,7 +8,7 @@ function bufferToBase64(bufferObj) {
     byteArray.forEach(byte => binary += String.fromCharCode(byte));
     return window.btoa(binary);
   }
-
+  
 const PopupPosts = ({ isOpen, onClose, posts, date }) => {
     if (!isOpen) return null; // si isOpen est nul la popup ne s'ouvre pas
     const [datePosts,setDatePosts] = useState(
@@ -22,7 +22,8 @@ const PopupPosts = ({ isOpen, onClose, posts, date }) => {
     //console.log(datePosts[0].scheduledfor.split("T")[0]);
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg relative">
+      <div className="bg-white p-6 rounded-lg shadow-lg relative max-h-[80vh] w-[600px] overflow-y-auto">
+
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-600 text-xl"
@@ -32,10 +33,9 @@ const PopupPosts = ({ isOpen, onClose, posts, date }) => {
 
         {datePosts.map((post, index) => {
   const media = post.mediaFiles?.[0];
-
   const image =
     media && media.data
-      ? `data:${media.contentType};base64,${bufferToBase64(media.data)}`
+      ? `data:${media.contentType};base64,${media.data}`
       : null;
 
   return (
