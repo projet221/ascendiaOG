28ac925db7b8644587ef56091f7cfd7e828ff2d1
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-04-17 16:53:38 +0200

    update socialAuthController.js

--- backend/services/socialAuth/controllers/socialAuthController.js
+++ backend/services/socialAuth/controllers/socialAuthController.js
@@ -171,7 +171,7 @@ const socialAuthController = {
 
     getSocialMediaProviderByUserId : async (req, res) => {
         try {
-            const socialAuth = await SocialAuth.find({user: req.params.user_id}).select('provider profile page');
+            const socialAuth = await SocialAuth.find({user: req.params.user_id}).select('provider profile pages');
             if (!socialAuth) {
                 return res.status(404).json({})
             }
