8ff013a80660e5e04a81bfefcf82f2b7fa800d6b
sami <sami.chbicheb@etu.umontpellier.fr>
2025-04-23 18:14:30 +0200

    ajout des posts avec leur previsualisation selon leur plateforme dans le calendrier

--- frontend/reactJS/src/components/PopupPosts.jsx
+++ frontend/reactJS/src/components/PopupPosts.jsx
@@ -80,13 +80,14 @@ const PopupPosts = ({ isOpen, onClose, posts, date }) => {
           <IoIosArrowBack />
         </button>
 
-        <div className="flex-1 max-w-[400px]">
-          <Previsualisation
-            platform={currentPlatform}
-            text={post.content}
-            image={image}
-          />
-        </div>
+           <div className="w-80 h-80 border rounded-lg overflow-auto flex flex-col p-2">
+  <Previsualisation
+    platform={currentPlatform}
+    text={post.content}
+    image={image}
+    className="flex-1"
+  />
+</div>
 
         <button
           onClick={nextPlatform}
