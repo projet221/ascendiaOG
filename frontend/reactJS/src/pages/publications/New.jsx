771cafbecc710326044e351dcef2891806e05bca
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-04-23 07:40:30 +0200

    test New.jsx

--- frontend/reactJS/src/pages/publications/New.jsx
+++ frontend/reactJS/src/pages/publications/New.jsx
@@ -292,7 +292,7 @@ function New() {
                                 </div>
                             )}
 
-
+                    </div>
                         </div>
                     </div>
 
