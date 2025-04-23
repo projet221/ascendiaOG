5de44a6cd5e75b801b355c6362066ad5aecaa328
Samir-Elkhoumri <samir.el-khoumri@etu.umontpellier.fr>
2025-04-23 05:15:30 +0200

    debug socialAuthController.js

--- backend/services/socialAuth/controllers/socialAuthController.js
+++ backend/services/socialAuth/controllers/socialAuthController.js
@@ -95,7 +95,7 @@ const socialAuthController = {
                     const pages_info = async (token) => {
                         try {
                             const response = await axios.get(`https://graph.facebook.com/v18.0/me/accounts?access_token=${token}`);
-
+                            console.log(" Grosse attention par icicccccccccccccccccccccccc : \n",response.data);
                             return response.data.data.map(item => ({ name: item.name, id: item.id }));
                         } catch (error) {
                             console.error("Erreur lors de la récupération des pages:", error);
